1878a3759357d1ccded167230323640f33ca3fe4
MuhammadYusuf <mohammedyousef.dev@gmail.com>
2024-01-10 17:06:56 +0500

    Reinitialized

--- app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from app.models import Images
-
-
-admin.site.register(Images)
--- app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app.views import index, HomePageView, get_random_images
-urlpatterns = [
-    path("", HomePageView.as_view()),
-    path("photo/", index, name="photo"),
-    path("image-post/", get_random_images),
-]
